2d4eeff31e287520c86560198e55bfcd450f0079
Guillaume Gruber <lagrube1@gmail.com>
2021-05-03 13:59:07 +0200

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://" +
+      process.env.DB_USER_PASS +
+      "@cluster0.hrvir.mongodb.net/e-commerce",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Error connexion", err));
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+
+const app = express();
+
+// routes
+
+// server
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
