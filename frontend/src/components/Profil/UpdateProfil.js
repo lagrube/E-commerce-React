41f2cc69800a5eff02fe0a413fd142d569a8a48c
Guillaume Gruber <lagrube1@gmail.com>
2021-07-26 18:00:50 +0200

    modif css post profil

--- frontend/src/components/Profil/UpdateProfil.js
+++ frontend/src/components/Profil/UpdateProfil.js
@@ -4,6 +4,7 @@ import { getPosts } from "../../actions/posts.actions";
 // import { getPosts } from "../../actions/posts.actions";
 import { updateBio } from "../../actions/user.actions";
 import LeftNavbar from "../LeftNavbar";
+import LikeButton from "../Post/LikeButton";
 import { dateParser, isEmpty } from "../Utils";
 import FollowHandler from "./FollowHandler";
 import UploadImg from "./UploadImg";
@@ -171,7 +172,7 @@ const UpdateProfil = () => {
         )}
         {userPosts && (
           <div className="post-container">
-            <h2>Posts</h2>
+            <h2>Vos derniers posts</h2>
             <span className="cross" onClick={() => setUserPosts(false)}>
               &#10005;
             </span>
@@ -184,9 +185,85 @@ const UpdateProfil = () => {
                     uploadUserPost.map((post) => {
                       for (let i = 0; i < uploadUserPost.length; i++) {
                         return (
-                          <div className="card-user" key={post._id}>
-                            <h3>{post.message}</h3>
-                          </div>
+                          <li className="card-container" key={post._id}>
+                            {isLoading ? (
+                              <i className="fas fa-spinner fa-spin"></i>
+                            ) : (
+                              <>
+                                <div className="card-left">
+                                  <img
+                                    src={usersData
+                                      .map((user) => {
+                                        if (user._id === post.posterId) {
+                                          return user.picture;
+                                        } else {
+                                          return null;
+                                        }
+                                      })
+                                      .join("")}
+                                    alt="poster-pic"
+                                  />
+                                </div>
+                                <div className="card-right">
+                                  <div className="card-header">
+                                    <div className="pseudo">
+                                      <h3>
+                                        {usersData
+                                          .map((user) => {
+                                            if (user._id === post.posterId) {
+                                              return user.pseudo;
+                                            } else {
+                                              return null;
+                                            }
+                                          })
+                                          .join("")}
+                                      </h3>
+                                      {post.posterId !== userData._id && (
+                                        <FollowHandler
+                                          idToFollow={post.posterId}
+                                          type={"card"}
+                                        />
+                                      )}
+                                    </div>
+                                    <span>{dateParser(post.createdAt)}</span>
+                                  </div>
+                                  <p>{post.message}</p>
+                                  {post.picture && (
+                                    <img
+                                      src={post.picture}
+                                      alt="post-pic"
+                                      className="card-pic"
+                                    />
+                                  )}
+                                  {post.video && (
+                                    <iframe
+                                      width="500"
+                                      height="300"
+                                      src={post.video}
+                                      frameBorder="0"
+                                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                      allowFullScreen
+                                      title={post._id}
+                                    ></iframe>
+                                  )}
+                                  <div className="card-footer">
+                                    <div className="comment-icon">
+                                      <img
+                                        src="./img/icons/message1.svg"
+                                        alt="comment"
+                                      />
+                                      <span>{post.comments.length}</span>
+                                    </div>
+                                    <LikeButton post={post} />
+                                    <img
+                                      src="./img/icons/share.svg"
+                                      alt="share"
+                                    />
+                                  </div>
+                                </div>
+                              </>
+                            )}
+                          </li>
                         );
                       }
                       return null;
